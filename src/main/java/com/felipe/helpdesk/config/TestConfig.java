88a5cd3ea87d2864b82d0f97b3fbd073bbd489a1
lobowww <felipelboss@gmail.com>
2025-10-01 13:47:28 -0300

    Retirada da injeção de dados da classe main

---
+++ src/main/java/com/felipe/helpdesk/config/TestConfig.java
@@ -0,0 +1,22 @@
+package com.felipe.helpdesk.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.felipe.helpdesk.services.DBService;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+	
+	@Autowired
+	private DBService dbService;
+	
+	@Bean
+	public boolean instaciaDB() {
+		this.dbService.instanciaDB();
+		return true;
+	}
+}
